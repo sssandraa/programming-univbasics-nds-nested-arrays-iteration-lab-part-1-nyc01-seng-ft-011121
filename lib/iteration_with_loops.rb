0c954daf2d0f911a6c5d2b61cf3b135bde15e119
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-02 16:51:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,6 +9,7 @@ if src[row_index][element_index].even?
   end
   row_index += 0
 end
+end
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
   # Output all even values in each nested array
