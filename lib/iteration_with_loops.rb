e4576f22c889590ddc69ad2f6b05acb9e32b5e20
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-02 16:51:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,11 +3,11 @@ row_index = 0
 while row_index < src.length do
   element_index = 0
   while element_index < src[row_index].length do
-if array_of_arrays[count][inner_count] / 2
-  p array_of_arrays[count][inner_count]
-  inner_count += 1
+if src[row_index][element_index].even?
+  p src[row_index][element_index]
+  element_index += 1
   end
-  count += 0
+  row_index += 0
 end
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
